f16c03ab00a0de8bf3cb41000f7f6593cc1738f6
dlm589 <46724026+dlm589@users.noreply.github.com>
2024-03-17 12:27:54 -0500

    Data processed and line chart built

---
+++ src/lib/line-chart.svelte
@@ -0,0 +1,431 @@
+<script>
+    import { scaleLinear } from "d3-scale"; // for scaling data to graph the data points
+    import jsondata from "../python/data-json/2023-ridership.json"; // import data
+    import { line, curveNatural } from "d3";
+    import "../assets/global-styles.css";
+   /*
+    export let mon; //get the selected variable name for 2021
+    export let tue; //get the selected variable name for 2021
+    export let colour96; // set colour for 1996
+    export let colour21; // set colour for 2021
+    export let transitName;
+    */
+    export let transitName
+    export let ridershiptype
+    let mon = `Monday-${ridershiptype}`
+    let tue = `Tuesday-${ridershiptype}`
+    let wed = `Wednesday-${ridershiptype}`
+    let thu = `Thursday-${ridershiptype}`
+    let fri = `Friday-${ridershiptype}`
+    let sat = `Saturday-${ridershiptype}`
+    let sun = `Sunday-${ridershiptype}`
+  
+    const lineColour = {
+      "淡水信義線": "#e3002c",
+      "松山新店線": "#008659",
+      "中和新蘆線": "#f8b61c",
+      "板南線": "#0070bd",
+      "環狀線": "#ffdb00",
+      "新北投支線": "#fd92a3",
+      "小碧潭支線": "#cfdb00",
+      "文湖線": "#c48c31"
+    };
+  
+    /* ======= SETTING UP DATA ==========*/
+  
+    //filtre data.json to the selected transitName in page.svelte
+    function filterDesignation(jsondata) {
+      return jsondata["Route"] === transitName;
+    }
+    console.log(transitName)
+    console.log(lineColour[transitName])
+    // store filtered jsondata in variable called data
+    var data = jsondata.filter(filterDesignation);
+  
+    /* ======= SETTING UP CHARTING AREAS ==========*/
+    const padding = { top: 20, right: 25, bottom: 10, left: 150 };
+    let width = 600; // set the base width
+    let height = data.length * 5 + padding.top + padding.bottom; // set the base width
+  
+    // adjust the width of the graph based on viewing size
+    $: innerWidth = width - (padding.left + padding.right);
+    $: innerHeight = height - (padding.top + padding.bottom);
+  
+    /* ======= SETTING UP X AND Y AXIS ========== */
+  
+    // ------ SETTING UP Y AXIS ------------------------
+    // create yTicks
+    const yTicks = data.map(function (obj) {
+      return obj["Code"];
+    });
+  
+    // ------ SETTING UP X AXIS ------------------------
+    // round the numbers for creating xTicks,
+    // create xticks using the newMax values
+    var xTicks = [5000, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 150000];
+  
+    // converts thousands and million to K and M i.e. (1,000 ==> 1K , 1,000,000 ==> 1M)
+    function thousandToK(tick) {
+      var newtick;
+      if (tick < 400) {
+        newtick = tick;
+      } else if (tick >= 1000 && tick < 1000000) {
+        newtick = tick / 1000 + "K";
+      } else if (tick > 1000000) {
+        newtick = tick / 1000000 + "M";
+      } else {
+        newtick = tick;
+      }
+      return newtick;
+    }
+  
+    // ------SETTING UP BARS FOR THE BAR CHART ------------------------
+    var barPadding = 1.5; // controls how much spacing the bars will be from the
+    // control barwidth based on viewing window width
+    $: barWidth = Math.max(Math.min(innerHeight / yTicks.length, 25), 6);
+  
+    /* ======= SCALING DATA POINTS ========== */
+    $: xScale = scaleLinear()
+      .domain([0, Math.max.apply(null, xTicks)])
+      .range([padding.left, innerWidth + padding.left - padding.right]);
+  
+    $: yScale = scaleLinear()
+      .domain([0, yTicks.length])
+      .range([padding.top + 10, height - padding.bottom - 2 * barWidth]); // added 0.2*height to accomodate station text label.
+  
+    /* ==== SCALING THE LINES === */
+    $: line_gen96 = line()
+      .curve(curveNatural)
+      .x((d) => xScale(d[mon]))
+      .y((d, i) => yScale(i) + barPadding + padding.top)(data);
+  
+    $: line_gen21 = line()
+      .curve(curveNatural)
+      .x((d) => xScale(d[sat]))
+      .y((d, i) => yScale(i) + barPadding + padding.top)(data);
+  
+    /* ======= SET UP DATA LABELLING ========== */
+    let selected_datapoint = undefined;
+    let selected_datapoint_i = undefined;
+  
+  
+    /* MOUSEOVER EVENT */
+    let mouse_x, mouse_y;
+  
+    const setMousePosition = function (event) {
+      mouse_x = event.clientX;
+      mouse_y = event.clientY;
+    };
+  
+    var axis = padding.left / 2.5;
+  </script>
+  
+  <div
+    id="barchart"
+    class="chart"
+    bind:clientHeight={height}
+    bind:clientWidth={width}
+  >
+    <svg
+      height={(data.length + 2) * (barWidth + barPadding) +
+        padding.top +
+        padding.bottom}
+      {width}
+    >
+      <!-- TEXT LABEL FOR THE STATION NAMES-->
+      <g>
+        {#each data as point, i}
+            <g class="station-tick">
+              <text
+                x={padding.left - 22}
+                y={yScale(i) + barPadding + padding.top + 5}
+                text-anchor="end"
+                font-size="14"
+              >
+                {point["Station"]}
+              </text>
+            </g>
+        {/each}
+      </g>
+      <path d={line_gen96} stroke={"white"} stroke-dasharray="6 3" />
+      <path d={line_gen21} stroke={"white"} />
+      <!-- CREATE THE LINES ON THE LINE GRAPH-->
+  
+      {#each data as point, i}
+        <!-- CREATE THE DOTS ON THE LINE GRAPH-->
+        <!-- {#if point[mon] !== null}
+          <circle
+            class="point"
+            r={2}
+            cx={xScale(point[mon])}
+            cy={yScale(i) + barPadding + padding.top}
+            fill={colour96}
+          />
+          <circle
+            class="point"
+            r={2}
+            cx={xScale(point[tue])}
+            cy={yScale(i) + barPadding + padding.top}
+            fill={colour21}
+          />
+        {/if} -->
+      {/each}
+  
+      <!-- CITY AVERAGE VALUES-->
+      <!-- <g>
+        <line class = "city-average"
+          x1={xScale(cityAverage[1][varName])}
+          y1={padding.top + 5}
+          x2={xScale(cityAverage[1][varName])}
+          y2={height - padding.bottom - 1.5 * barWidth}
+          stroke={"grey"}
+          stroke-width="1"
+        />
+        <text class = "city-average-text"
+          x={xScale(cityAverage[1][varName])+10}
+          y={padding.top + 50}
+        >2021 City Average: {cityAverage[1][varName]}</text>
+      </g> -->
+  
+      <!-- X AND Y AXIS-->
+      <!-- x axis -->
+      <line
+        x1={padding.left}
+        y1={padding.top + 4}
+        x2={padding.left + innerWidth}
+        y2={padding.top + 4}
+        stroke-width={1}
+        stroke={"#fff"}
+      />
+      {#if innerWidth > 300}
+        {#each xTicks as tick, i}
+          <text class="text" x={xScale(tick)} y={padding.top} text-anchor="middle"
+            >{thousandToK(tick)}
+          </text>
+          <line
+            x1={xScale(tick)}
+            y1={padding.top + 4}
+            x2={xScale(tick)}
+            y2={padding.top + 12}
+            stroke-width={1}
+            stroke={"#fff"}
+          />
+          <line
+            x1={xScale(tick)}
+            y1={padding.top + 30}
+            x2={xScale(tick)}
+            y2={height - padding.bottom - 2 * barWidth}
+            stroke-width={1}
+            stroke={"#4d4d4d"}
+          />
+        {/each}
+      {/if}
+      <!-- y axis -->
+      <line
+        class="axis y-axis"
+        x1={padding.left - 12}
+        y1={padding.top + 30}
+        x2={padding.left - 12}
+        y2={height - padding.bottom - 2 * barWidth}
+        stroke-width={6}
+        stroke={lineColour[transitName]}
+        z-index="3"
+      />
+  
+      <!-- CREATE STATION TICKS AND LABELS-->
+  
+      {#each data as point, i}
+        <!-- CREATE THE BARS ON THE LINE GRAPH-->
+        <!-- <rect
+          class="barLight"
+                  fill={"white"}
+          x={padding.left}
+          y={yScale(i) + barWidth / 2 - 2 * barPadding}
+          width={xScale(Math.max(point[tue])) - padding.left}
+          height={barWidth}
+          opacity="0.1"
+        />
+        <rect
+          class="barLight"
+                  
+          x={padding.left}
+          y={yScale(i) + barWidth / 2 - 2 * barPadding}
+          width={xScale(Math.max(point[tue])) - padding.left}
+          height={barWidth}
+          opacity="0"
+          on:mouseover={(event) => {
+            selected_datapoint = point;
+            setMousePosition(event);
+            document.querySelector(`#var-text-${i}`).style.opacity = 1;
+            document.querySelector(`#var-hover-${i}`).style.opacity = 1;
+            selected_datapoint_i = i;
+          }}
+          on:mouseout={() => {
+            // Reset the opacity of var-text when the mouse leaves the bar
+            document.querySelector(`#var-text-${i}`).style.opacity = 0;
+            document.querySelector(`#var-hover-${i}`).style.opacity = 0;
+          }}
+        /> -->
+        <!-- LABELLING THE DATA WILL SHOW ON HOVERING THE BARS-->
+        <!-- <rect
+          id={`var-hover-${i}`}
+          class="var-hover"
+          x={padding.left + innerWidth}
+          y={yScale(i) + barWidth / 2 - 2 * barPadding}
+          width={500}
+          height={barPadding + 3 * barWidth}
+          opacity="0"
+        />
+        <text
+          id={`var-text-${i}`}
+          class="var-text"
+          x={padding.left + innerWidth}
+          y={yScale(i) + barPadding + padding.top + 5}
+          >
+          <tspan
+            x={[xScale(Math.max(point[tue])) + 5]}
+                      dy={-8}
+            fill={lineColour[transitName]}
+            >{"1996: " + Math.round(point[mon] * 100) / 100}</tspan
+          >
+          <tspan
+            x={xScale(Math.max(point[tue])) + 5}
+            dy={15}
+            fill={lineColour[transitName]}
+            >{"2021: " + Math.round(point[tue] * 100) / 100}</tspan
+          >
+        </text> -->
+      {/each}
+  
+      <!-- Line that marks the location of the station-->
+      <g class="station-lines">
+        {#each data as point, i}
+            <line
+              class="station-sep-lines"
+              x1={padding.left + innerWidth}
+              y1={yScale(i) + barPadding + padding.top}
+              x2={padding.left}
+              y2={yScale(i) + barPadding + padding.top}
+              stroke-width={1}
+              stroke="#fff"
+              stroke-dasharray="5 3"
+              opacity="0.1"
+            />
+            <circle
+              class="point"
+              r={6}
+              cx={padding.left - 12}
+              cy={yScale(i) + barPadding + padding.top}
+              stroke-width="4px"
+              stroke={lineColour[transitName]}
+              fill="#FFFFFF"
+            />
+            {console.log(lineColour[transitName])}
+  
+        {/each}
+      </g>
+    </svg>
+  </div>
+  
+  <style>
+    /* CHART */
+    .chart {
+      /* width: 80%; */
+      /* left: 0%; */
+      max-width: 1000px;
+      min-width: 500px;
+    }
+  
+    svg {
+      position: relative;
+      /* width: 90%; */
+      /* left: 5%; */
+      z-index: 2;
+    }
+  
+    .city-average {
+      stroke-width: 5px;
+      z-index: 1;
+      stroke: white;
+      opacity: 0.5;
+    }
+    .city-average-text {
+      font-size: 15px;
+      font-weight: bold;
+      fill: #ffffff;
+    }
+  
+    /* XY axis */
+    .text {
+      fill: white;
+      font-size: 16px;
+      background-color: lightgrey;
+      font-weight: bold;
+    }
+  
+    /* AXIS */
+    .station-lines {
+      stroke-width: 5px;
+    }
+  
+    .station-tick {
+      text-anchor: right;
+      word-wrap: break-word;
+      fill: var(--brandGray);
+      font-size: 17px;
+      text-align: right;
+      max-width: 15px; /* Adjust the value as needed */
+      /* Optional: Enable text wrapping */
+      white-space: pre-line; /* Allow line breaks */
+    }
+    .station-box {
+      width: 200px;
+      height: 300px;
+      background-color: red;
+    }
+  
+    /* LINES GRAPH */
+    .point {
+      cursor: pointer;
+    }
+    .point:hover {
+      fill: var(--brandLightBlue);
+      opacity: 1;
+    }
+  
+    /* BAR GRAPH */
+    .barLight {
+      z-index: -5;
+    }
+  
+    .barLight:hover {
+      opacity: 0;
+      cursor: pointer;
+      z-index: 1;
+    }
+  
+    /* HOVERING */
+    #hoverLabel {
+      height: 30px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  
+    .var-text {
+      opacity: 0;
+      padding-top: 5px;
+      padding-left: 14px;
+      font-size: 12px;
+      font-weight: bold;
+    }
+    .var-hover {
+      fill: black;
+    }
+    path {
+      fill: transparent;
+      stroke-width: 2;
+      stroke-linejoin: round;
+      z-index: 40;
+    }
+  </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,262 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    //import TopSofC from "../lib/TopSofC.svelte";
+    import LineChart from "../lib/line-chart.svelte";
+    import "../assets/global-styles.css";
+    //import sofcLongCity from "../assets/sofc-long-city.svg";
+
+    let transitName = "淡水信義線";
+    var variableName = "Population";
+
+    var censusItems = [
+        "Population",
+        "Weighted Average Total Individual Income",
+        "Weighted Average Total Household Income",
+        "Total Occupied Dwellings",
+        "Single-detached house",
+        "Semi-detached house",
+        "Row house",
+        "Apartment or flat in a -plex*",
+        "Apartment fewer than five storeys",
+        "Apartment five or more storeys",
+        // "Weighted Average number of rooms per dwelling",
+        // "Owner",
+        // "Renter",
+        // "Owner Renter Ratio",
+    ];
+
+    var transit_lines = [
+        "淡水信義線",
+        "松山新店線",
+        "中和新蘆線",
+        "板南線",
+        "環狀線",
+        "新北投支線",
+        "小碧潭支線",
+        "文湖線",
+    ];
+    function handleTransitChange(event) {
+        const selectedValue = event.target.value;
+        console.log(selectedValue);
+        transitName = selectedValue;
+        //selectedVariable.set(selectedValue);
+    }
+    function handleCensusChange(event) {
+        const selectedValue = event.target.value;
+        //console.log(selectedValue);
+        variableName = selectedValue;
+        //selectedVariable.set(selectedValue);
+    }
+    const lineColour = {
+      "淡水信義線": "#e3002c",
+      "松山新店線": "#008659",
+      "中和新蘆線": "#f8b61c",
+      "板南線": "#0070bd",
+      "環狀線": "#ffdb00",
+      "新北投支線": "#fd92a3",
+      "小碧潭支線": "#cfdb00",
+      "文湖線": "#c48c31"
+    };
+
+    let topWidth = 200;
+</script>
+
+<!--<TopSofC />-->
+
+<main>
+    <div class="title" bind:clientWidth={topWidth}>
+        <!--<img width=100% height=71 src={sofcLongCity} alt="sofcLongCity" >-->
+
+        <svg width="100%" height="20">
+            <line
+                x1="2"
+                y1="10"
+                x2={topWidth - 30 - 5}
+                y2="10"
+                stroke={lineColour[transitName]}
+                stroke-width="5"
+                stroke-linecap="round"
+            />
+            {#each [0, 1, 2, 3] as c}
+                <circle
+                    r={6}
+                    cx={8 + c * ((topWidth - 30 - 16) / 3)}
+                    cy={10}
+                    stroke-width="4px"
+                    stroke={lineColour[transitName]}
+                    fill="#fff"
+                />
+            {/each}
+        </svg>
+
+        <h1>
+            25 Years of Transit-Oriented Development in Toronto: What Has
+            Changed?
+        </h1>
+
+        <p>
+            <a href="">Michael Liu</a> and <a href="">Jeff Allen</a>
+        </p>
+    </div>
+
+    <div class="text">
+        <p>
+            Toronto has had mixed results in encouraging building new housing
+            units where there is easy access to transit services, especially
+            along subway and light-rail lines.
+        </p>
+        <p>
+            We've created an interactive chart for exploring Transit-Oriented
+            Development (TOD), or lack there-of, along Toronto's existing and
+            planned major transit lines, specifically for areas within 800
+            metres (approx. a 10-15 minute walk) from these lines. The dropdowns
+            allow for selecting between lines and different aspects of TOD and
+            it's impact on neighbourhoods, including population, dwelling types,
+            and income. The chart has two curves, one for 1996 and one for 2021,
+            to compare and show changes over this 25 year period.
+        </p>
+        <p>
+            <select
+                id="transit"
+                value={transitName}
+                on:change={handleTransitChange}
+            >
+                {#each transit_lines as value}
+                    <option {value}>{value}</option>
+                {/each}
+            </select>
+        </p>
+        <p>
+            <select
+                id="census"
+                value={variableName}
+                on:change={handleCensusChange}
+            >
+                {#each censusItems as value}
+                    <option {value}>{value}</option>
+                {/each}
+            </select>
+        </p>
+
+        <p>
+            The chart is currently showing the <b>{variableName}</b> that is
+            within an <b>800m</b> distance from each point on the
+            <b>{transitName}</b>
+        </p>
+    </div>
+
+    <!-- key is a function that would destroy the element and rebuilt it upon variable change-->
+    {#key [transitName, variableName]}
+        <div class="chart">
+            <LineChart transitName={transitName} ridershiptype={"exit"} />
+        </div>
+    {/key}
+
+    <div class="text">
+        <h2>What Is Transit Oriented Development?</h2>
+        <p>
+            Transit-oriented development (TOD) is a way of building the cities
+            transit-infrastructures. A TOD project is usually higher density,
+            meaning around transit-infrastructures. A TOD project is usually
+            higher that it is a more efficient use of land to house people, jobs
+            and other density, meaning that it is a more efficient use of land
+            to house activities. When TOD is applied to a community level, the
+            area would be people, jobs and other activities. When TOD is applied
+            to a built to be pedestrian friendly so its residents can have easy
+            access to community level, the area would be built to be pedestrian
+            friendly public transit. The area would also be amenity rich,
+            providing a range of so its residents can have easy access to public
+            transit. The area services and job opportunities. Through TOD, a
+            community will become a place would also be amenity rich, providing
+            a range of services and job where people can live, work, and play.
+            opportunities. Through TOD, a community will become a place where
+            people can live, work, and play.
+
+            <br />
+            <br /><b>What is the benefit of TOD? </b><br />
+
+            TOD allows people to reduce their reliance on cars for many errands.
+            For instance, a resident can simply walk to a nearby grocery store
+            to get groceries, bike a few blocks to get to the place where they
+            are employed, and access the park nearby, all without having the
+            need to rely on cars. Through reducing reliance on cars, people will
+            not have to allocate a portion of their income to buy and operate a
+            car. TOD encourages people to walk and bike more, which can in turn
+            help people exercise, reducing the level of obesity and the diseases
+            caused by it. It is also beneficial to our environment as we reduce
+            the amount of carbon emissions and harmful air pollution particles
+            that would cause respiratory issues.
+            <br /> <br />
+            TOD is also beneficial to city governments, as TOD would locate a large
+            number of residents and jobs, which would encourage people to use public
+            transit and justify the cost of operating these systems. By building
+            more densely, cities can also cut operation and maintenance costs of
+            their infrastructures, such as roads, sewage, water, and waste collection.
+        </p>
+
+        <h2>Methodology</h2>
+        <p>
+            Transit-Oriented Developments are defined as areas within 5-10
+            minutes walk to a transit service. In Ontario, this is equivalent to
+            500 to 800 metres. This widget compares data from <a
+                href="https://www12.statcan.gc.ca/census-recensement/index-eng.cfm"
+                >Statistics Canada’s
+            </a>
+            Census for the year of 1996 and 2021. Census data agglomerates data to
+            multiple geographical areas, from city level data to the smallest and
+            publicly available level, Dissemination Area level data, which is consisted
+            of a few city blocks. This widget uses Dissemination Area to get the
+            most accurate estimate of the census variables. Due to the boundary of
+            800-metre buffers do not align with Dissemination Area boundaries, we
+            used areal interpolation to estimate the density of each variable within
+            respective Dissemination Areas, then interpolate the data by the area
+            that is within the 800 metre buffer. <br />
+        </p>
+        <p>
+            We generated points every 400 meters along transit-services in
+            Toronto, then created 800-meter buffers around the generated points.
+            Since transit stations are not 400m apart, We also used Thiessen
+            Polygon to determine the closest station for each of the point we
+            generated.
+            <br />
+        </p>
+        <p>
+            All transit layers, including the transit line and transit stations,
+            were downloaded from <a
+                href="https://www.metrolinx.com/en/about-us/open-data"
+            >
+                Metrolinx's Open Data Catalogue</a
+            >
+        </p>
+    </div>
+</main>
+
+<style>
+    select {
+        padding: 3px;
+        padding-left: 5px;
+        width: 340px;
+        background-color: var(--brandBlack);
+        border: 1px solid var(--brandBlack);
+        color: white;
+        font-size: 16px;
+        border-color: var(--brandGray70);
+        border-radius: 1px;
+    }
+    select option {
+        background-color: var(--brandGray90);
+        color: white;
+        stroke: white;
+    }
+
+    select:hover {
+        cursor: pointer;
+        background-color: var(--brandDarkBlue);
+    }
+
+    .chart {
+        max-width: 1046px;
+        background-color: none;
+        margin: 0 auto;
+        position: relative;
+    }
+</style>
